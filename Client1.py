d2fc5359772342da56f094b8645dbd2f5c393b92
Shahar <Shaharhadad123@gmail.com>
2021-12-29 18:33:59 +0200

    send end game message to clients and close connections

--- Client1.py
+++ Client1.py
@@ -3,6 +3,7 @@ import socket
 import struct
 import getch
 import socket
+import time
 
 group_name = "Pytonic1"
 udp_port = 13117
@@ -15,7 +16,7 @@ def get_broadcast_message(udp_client):
     # udp_client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     try:
         data, addr = udp_client.recvfrom(1024)
-        msg = struct.unpack('Ibh', data)
+        msg = struct.unpack('IbH', data)
     except Exception as e:
         # print(e)
         return (-1, -1)
@@ -27,33 +28,43 @@ def get_broadcast_message(udp_client):
 
 
 if __name__ == "__main__":
-    # UDP
-    udp_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp_client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    udp_client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    udp_client.bind(("", udp_port))
-    print("Client started, listening for offer requests...")
     while True:
-        server_address = get_broadcast_message(udp_client)
-        print(int(server_address[1]))
-        if int(server_address[1]) == 13000:  # and int(server_address[1]) == 13000:
-            break
+        # UDP
+        udp_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        udp_client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        udp_client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        udp_client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        udp_client.bind(("", udp_port))
+        print("Client started, listening for offer requests...")
+        while True:
+            server_address = get_broadcast_message(udp_client)
+            print(int(server_address[1]))
+            if int(server_address[1]) > 1024 and int(server_address[1]) != 13000:
+                break
 
-    #    print(server_address)
+        #    print(server_address)
 
-    # TCP
-    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        tcp_socket.connect(server_address)
-    except Exception as e:
-        print(e)
-        exit(2)
-    group_name_bytes = bytes(group_name + "\n", encoded)
-    tcp_socket.sendall(group_name_bytes)
-    print(tcp_socket.recv(1024).decode(encoded))
-    key_num = getch.getch()
-    tcp_socket.send(bytes(key_num, encoded))
-    tcp_socket.recv(1024).decode(encoded)
+        # TCP
+        tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            tcp_socket.connect(server_address)
+        except Exception as e:
+            print(e)
+            udp_client.close()
+            continue
+        udp_client.close()
+        # send the group name to server
+        group_name_bytes = bytes(group_name + "\n", encoded)
+        tcp_socket.sendall(group_name_bytes)
+        print(tcp_socket.recv(1024).decode(encoded))
+        # get a key input
+        key_num = getch.getch()
+        tcp_socket.send(bytes(key_num, encoded))
+        # tcp_socket.settimeout(0.01)
+        print(tcp_socket.recv(1024).decode(encoded))
+        print(tcp_socket.recv(1024).decode(encoded))
+
+        tcp_socket.close()
 
 # udp_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # udp_client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
--- Server.py
+++ Server.py
@@ -7,6 +7,7 @@ from concurrent.futures import ThreadPoolExecutor as pool
 import multiprocessing
 import threading
 import random
+import select
 
 udp_port = 13117
 tcp_port = 13000
@@ -16,7 +17,7 @@ clients_ans = []
 
 
 def send_broadcast(udp_socket):
-    msg = struct.pack("Ibh", 2882395322, 2, tcp_port)
+    msg = struct.pack("IbH", 2882395322, 2, tcp_port)
     udp_socket.sendto(msg, ("<broadcast>", udp_port))
     # time.sleep(1)
 
@@ -28,13 +29,11 @@ def thread_broadcast(udp):
 
 def create_random_equation():
     operator_list = ["+", "-"]
-    equation_numbers = random.randint(2, 4)
-    eq = str(random.randint(1, 20))
+    # equation_numbers = random.randint(2,4)
+    equation_numbers = 2
+    eq = str(random.randint(0, 4))
     for i in range(1, equation_numbers):
-        oper_index = random.randint(0, 1)
-        print(f"oper_index: {oper_index}")
-        eq += operator_list[oper_index]
-        eq += str(random.randint(1, 10))
+        eq += "+" + str(random.randint(0, 4))
     return eq
 
 
@@ -45,96 +44,90 @@ def listening_to_client(item):
     clients_ans.append((item[0], num_from_client))
 
 
-# def send_broadcast(udp_socket):
-#     msg = struct.pack("Ibh", 2882395322, 2, tcp_port)
-#     udp_socket.sendto(msg, ("<broadcast>", udp_port))
-#     time.sleep(1)
+def send_end_game_msg(socket, msg):
+    print(socket)
+    byte_msg = bytes(msg, encoded)
+    print("before msg")
+    socket.sendall(byte_msg)
+    print("after msg")
+
+
+# (group name, socket)
+def closed_clients_sockets(tcp_socket_list):
+    global clients_ans
+    for item in tcp_socket_list:
+        item[1].close()
+    clients_ans = []
 
 
 if __name__ == "__main__":
-    # UDP
-    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-    print(f"Server started, listening on IP address {server_ip}")
-    # num_of_clients = 0
-    # while num_of_clients < 2:
-    #     send_broadcast(udp_socket)
-    sending = multiprocessing.Process(target=thread_broadcast, args=(udp_socket,))
-    sending.start()
-
-    # TCP
-    tcp_socket_clients = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    tcp_socket_clients.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-    tcp_socket_clients.bind(("", tcp_port))
-    tcp_socket_clients.listen(2)
-    tcp_socket_list = []
-    # with pool(max_workers=2):
-    while len(tcp_socket_list) < 2:
-        (tcp_socket, addr) = tcp_socket_clients.accept()
-        group_name = tcp_socket.recv(1024).decode(encoded)
-        tcp_socket_list.append((group_name, tcp_socket))
-        print("************************")
-        print(addr)
-
-    udp_socket.close()
-    # (tcp_socket2, addr2) = tcp_socket_clients.accept()
-    # group_name2 = tcp_socket2.recv(1024).decode(encoded)
-    # tcp_socket_list.append((group_name2, tcp_socket2))
-    # print("************************")
-    # print(group_name2)
-    welcome_msg = f"Welcome to Quick Maths.\nPlayer 1: {tcp_socket_list[0][0]}Player 2: {tcp_socket_list[1][0]}==\nPlease answer the following question as fast as you can:\n"
-    eq = create_random_equation()
-    eq_ans = eval(eq)
-    print(eq_ans)
-    welcome_msg += eq
-    print(welcome_msg)
-    welcome_msg_bytes = bytes(welcome_msg, encoded)
-
-    tcp_socket_list[0][1].send(welcome_msg_bytes)
-    tcp_socket_list[1][1].send(welcome_msg_bytes)
-    t_client1 = threading.Thread(target=listening_to_client, args=[tcp_socket_list[0]])
-    t_client2 = threading.Thread(target=listening_to_client, args=[tcp_socket_list[1]])
-    start_game_time = time.time()
-    # without this sleep it will terminated
-    time.sleep(0.5)
-    t_client1.start()
-    t_client2.start()
-
     while True:
-        # without this sleep it will terminated
-        time.sleep(0.2)
-        if len(clients_ans) > 0 or time.time() - start_game_time > 10:
-            break
+        tcp_socket_list = []
+        # UDP
+        udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        print(f"Server started, listening on IP address {server_ip}")
+        sending = multiprocessing.Process(target=thread_broadcast, args=(udp_socket,))
+        sending.start()
 
-    end_game_msg = f"Game over!\nThe correct answer was {eq_ans}!\n\n"
+        # TCP
+        tcp_socket_clients = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcp_socket_clients.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        tcp_socket_clients.bind(("", tcp_port))
+        tcp_socket_clients.listen(2)
 
-    if len(clients_ans) == 0:
-        msg = "The game ended with a draw"
-        print(msg)
-    else:
-        if int(clients_ans[0][1]) == eq_ans:
-            winner = clients_ans[0][0]
+        while len(tcp_socket_list) < 2:
+            (tcp_socket, addr) = tcp_socket_clients.accept()
+            group_name = tcp_socket.recv(1024).decode(encoded)
+            tcp_socket_list.append((group_name, tcp_socket))
+        udp_socket.close()
+        welcome_msg = f"Welcome to Quick Maths.\nPlayer 1: {tcp_socket_list[0][0]}Player 2: {tcp_socket_list[1][0]}==\nPlease answer the following question as fast as you can:\n"
+        eq = create_random_equation()
+        eq_ans = eval(eq)
+        welcome_msg += eq
+        print(welcome_msg)
+        welcome_msg_bytes = bytes(welcome_msg, encoded)
+
+        tcp_socket_list[0][1].send(welcome_msg_bytes)
+        tcp_socket_list[1][1].send(welcome_msg_bytes)
+        t_client1 = threading.Thread(target=listening_to_client, args=[tcp_socket_list[0]])
+        t_client2 = threading.Thread(target=listening_to_client, args=[tcp_socket_list[1]])
+        start_game_time = time.time()
+        while True:
+            print(clients_ans)
+            # without this sleep it will terminated
+            time.sleep(0.2)
+            if len(clients_ans) > 0 or time.time() - start_game_time > 10:
+                break
+            try:
+                t_client1.start()
+                t_client2.start()
+            except Exception as e:
+                pass
+
+        end_game_msg = f"Game over!\nThe correct answer was {eq_ans}!\n\n"
+        print(clients_ans)
+        if len(clients_ans) == 0:
+            msg = "The game ended with a draw"
+            print(msg)
         else:
-            for item in tcp_socket_list:
-                if item[0] != clients_ans[0][0]:
-                    winner = item[0]
-        print(f"the winnder team is: {winner}")
-        msg = f"Congratulations to the winner: {winner}"
-    end_game_msg += msg
-    print(end_game_msg)
+            winner = ""
+            if clients_ans[0][1] == str(eq_ans):
+                winner = clients_ans[0][0]
+            else:
+                for item in tcp_socket_list:
+                    if item[0] != clients_ans[0][0]:
+                        winner = item[0]
+            print(f"the winnder team is: {winner}")
+            msg = f"Congratulations to the winner: {winner}"
+        end_game_msg += msg
+        print(end_game_msg)
+        t_client3 = threading.Thread(target=send_end_game_msg, args=[tcp_socket_list[0][1], end_game_msg])
+        t_client4 = threading.Thread(target=send_end_game_msg, args=[tcp_socket_list[1][1], end_game_msg])
+        t_client3.start()
+        t_client4.start()
 
-    # else:
-    #     # need to implement it
-    #     winner = check_ans(clients_ans, tcp_socket_list)
-    #     msg = winner
-
-    # num_from_client1 = tcp_socket_list[0][1].recv(1024).decode(encoded)
-    # num_from_client2 = tcp_socket_list[1][1].recv(1024).decode(encoded)
-
-    # print(f"client1 number is: {num_from_client1} and client2 number is: {num_from_client2}")
-
-    # if eval == int(num_from_client1):
-    #     print(f"{tcp_socket_list[0][0]} wins")
-    # else:
-    #     print(f"{tcp_socket_list[1][0]} wins")
+        print(f"Game over, sending out offer requests...")
+        time.sleep(0.5)
+        closed_clients_sockets(tcp_socket_list)
